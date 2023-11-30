113525add875e4ae6fccb606b8d06d93fcc3ef47
emiliastx <148064714+emiliastx@users.noreply.github.com>
2023-11-30 16:03:21 +0000

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#current-temp");
+  let temperature = response.data.main.temp;
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "62231151ce343c4d68652e1617efc22f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function controlSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let currentCityElement = document.querySelector("#current-city");
-  currentCityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", controlSearchSubmit);
+
+searchCity("London");
