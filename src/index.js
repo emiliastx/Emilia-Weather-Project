a8f4fdbc0f51e88237228f00ab5887c7a894fbba
emiliastx <148064714+emiliastx@users.noreply.github.com>
2023-12-01 18:47:33 +0000

    Added more weather data
    Added more real-time weather data, including wind speed, humidity, date/time

--- src/index.js
+++ src/index.js
@@ -2,8 +2,39 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#current-temp");
   let temperature = response.data.main.temp;
   let currentCityElement = document.querySelector("#current-city");
+  let currentWeatherElement = document.querySelector("#current-weather-type");
+  let currentHumidityElement = document.querySelector("#current-humidity");
+  let currentWindElement = document.querySelector("#current-wind");
+  let currentTimeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.dt * 1000);
+
+  console.log(response.data);
+
   currentCityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(temperature);
+  currentWeatherElement.innerHTML = response.data.weather[0].main;
+  currentHumidityElement.innerHTML = response.data.main.humidity;
+  currentWindElement.innerHTML = response.data.wind.speed;
+  currentTimeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
