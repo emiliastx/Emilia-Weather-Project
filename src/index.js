e5ff2567ebe177fd625f970420613d5d1932a2f3
emiliastx <148064714+emiliastx@users.noreply.github.com>
2023-12-06 12:31:17 +0000

    Moved forecast HTML into Javascript

--- src/index.js
+++ src/index.js
@@ -59,3 +59,33 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", controlSearchSubmit);
 
 searchCity("London");
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="row">
+    <div class="col-2 column">
+      <div class="weather-forecast-date">${day}</div>
+      <img
+        src="https://openweathermap.org/img/wn/04d@2x.png"
+        alt="day-one-icon"
+        width="70px"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">12°</span>
+        <span class="weather-forecast-temperature-min">3°</span>
+      </div>
+    </div>
+  </div>
+`;
+  });
+  forecastElement.innerHTML = forecastHtml;
+}
+displayForecast();
